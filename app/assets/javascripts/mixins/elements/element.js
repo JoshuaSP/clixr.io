bb3dac3f8653afff690ae5166c60ff16b706c09a
Joshua Penman <joshua.s.penman@gmail.com>
2015-04-09 14:17:42 -0700

    moving towards drag-box

--- app/assets/javascripts/mixins/elements/element.js
+++ app/assets/javascripts/mixins/elements/element.js
@@ -3,6 +3,17 @@ ClixrIo.Views.Element = Backbone.View.extend({
     "click": "selectElement"
   },
 
+  handles: {
+    "n": { left: "50%", top: "-5px"},
+    "ne": { right: "-5px", top: "-5px"},
+    "e": { right: "-5px", top: "50%"},
+    "se": { right: "-5px", bottom: "-5px"},
+    "s": { right: "50%", bottom: "-5px"},
+    "nw": { left: "-5px", top: "-5px"},
+    "w": { left: "-5px", top: "50%"},
+    "sw": { left: "-5px", bottom: "-5px"}
+  },
+
   initialize: function () {
     this.className = this.model.get('class'),
     this.model.css = this.model.get('css') ? $.parseJSON(this.model.get('css')) : {} ;
@@ -16,8 +27,16 @@ ClixrIo.Views.Element = Backbone.View.extend({
   },
 
   selectElement: function () {
-    this.$el.draggable().resizable({ handles: "all" });
-    this.setCss("border", "2px solid " + ClixrIo.Constants.SelectBoxColor);
+    var $handles = {};
+    this.$el.draggable();
+    for (var handle in this.handles) {
+      var circle = $('<div class="drag-handle">');
+      circle.addClass("ui-resizable-handle ui-resizable-" + handle);
+      circle.css(this.handles[handle]);
+      $handles[handle] = circle;
+    }
+    this.$el.resizable($handles);
+    this.setCss("border", "1px solid " + ClixrIo.Constants.SelectBoxColor);
   }
 });
 
