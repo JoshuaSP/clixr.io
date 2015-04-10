1886787cfe725fa890bf8e443490f1a1e30f380e
Joshua Penman <joshua.s.penman@gmail.com>
2015-04-10 00:02:39 -0700

    moving towards working textclick

--- app/assets/javascripts/views/elements/text.js
+++ app/assets/javascripts/views/elements/text.js
@@ -6,26 +6,34 @@ ClixrIo.Views.Text = ClixrIo.Views.Element.extend({
     this.$el.html(this.model.escape('content'));
   },
 
-  secondClick: function () {
-    if (this.toolbar) return;
-    // this next bit just for now
-    this.$el.resizable('destroy').draggable('destroy');
-    // we'll fix it when we have proper submenus
+  selectElement: function () {
+    if (this.selected) {
+      this.secondClick();
+      return;
+    }
+    this.$el.addClass("selected-element");
+    this.selected = true;
+    this.$el.draggable();
+    var $handles = this._addResizeHandles();
+    this.$el.resizable({ handles: $handles });
     this.$el.attr('id', 'texteditor');
     this._setupToolbar();
     this.editor = new wysihtml5.Editor("texteditor", {
        toolbar: "wysihtml5-toolbar",
        parserRules: wysihtml5ParserRules, // defined in parser rules set
     });
-    this.$el.attr('tabindex', 0)
-    this.$el.focus();
-    this.$el.trigger('click');
   },
 
-  _setupToolbar: function () {
-    this.toolbar = $(JST['menus/text_toolbar']());
-    var menusContainer = $('.floating-menus');
-    menusContainer.append(this.toolbar);
+  // we set up the editor one click before we need to edit because reasons.
+  // because we can't programmatically get the cursor in the box.
+  // we really tried.
+
+  secondClick: function () {
+    this.$el.draggable('destroy');
+    this._showToolbar();
+  },
+
+  _showToolbar: function () {
     this.toolbar.draggable();
     this.toolbar.position({
       my: "center",
@@ -33,6 +41,14 @@ ClixrIo.Views.Text = ClixrIo.Views.Element.extend({
       of: this.$el
       // within: menusContainer
     });
+    this.toolbar.css("display", "block");
+  },
+
+  _setupToolbar: function () {
+    this.toolbar = $(JST['menus/text_toolbar']());
+    var menusContainer = $('.floating-menus');
+    menusContainer.append(this.toolbar);
+    this.toolbar.css("display", "none");
   },
 
   deselectElement: function () {
