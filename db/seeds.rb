49b941eadeee2098cb5caea19c92a6e1bdad814b
Joshua Penman <joshua.s.penman@gmail.com>
2015-04-13 15:34:08 -0700

    add basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Site.create!({
+   user_id: 0,
+   title: "Blank Site",
+   background_url:
+    "http://www.beingdevelopers.com/wp-content/uploads/2014/04/html-background-1716x700_c.jpg"
+})
+
+Page.create!([{
+  site_id: 1,
+  title: "Page 1",
+  ord: 0
+}, {
+  site_id: 1,
+  title: "Second Page",
+  ord: 1
+}, {
+  site_id: 1,
+  title: "Page C",
+  ord: 2
+}, {
+  site_id: 1,
+  title: "IVeme Page",
+  ord: 3
+}])
