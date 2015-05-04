8c46a2db9ce3b44e81e970247ae90326810f4fb7
JoshuaSP <joshua.s.penman@gmail.com>
2015-05-04 02:05:27 -0700

    fix scrolling problem

--- app/controllers/api/sites_controller.rb
+++ app/controllers/api/sites_controller.rb
@@ -1,6 +1,6 @@
 class Api::SitesController < ApplicationController
   BAD_ADDRESSES = ["api", "keep_alive", "sites", "edit"]
-  DISALLOWED_REGEX = RegExp.new ("[^A-Za-z]" + BAD_ADDRESSES.map do |bad_address|
+  DISALLOWED_REGEX = Regexp.new ("[^A-Za-z]" + BAD_ADDRESSES.map do |bad_address|
     "|^#{bad_address}$|"
   end.join("") + "^$")
 
